bc8f2ab0e00a114bf4d594c8eedebf5495d35af0
Marcos Vermolhen <marcosberlim777@gmail.com>
2025-07-28 22:03:24 -0300

    Aula1.PHP

---
+++ conta.php
@@ -0,0 +1,31 @@
+<?php
+// Código PHP estruturado
+// $preco = 100;
+// $desconto = 0.1;
+// $final = $preco - ($preco * $desconto);
+// echo $final;
+// Resultado: 90
+
+/* Exemplo de classe em PHP
+ * A classe Produto tem dois atributos: preco e desconto.
+class Produto {
+ public $preco;
+ public $desconto;
+
+ * O método precoFinal calcula o preço final aplicando o desconto.
+ public function precoFinal() {
+ return $this->preco - 
+ ($this->preco * $this->desconto);
+ }
+}
+* Uso da classe
+$camiseta = new Produto();
+$camiseta->preco = 100;
+$camiseta->desconto = 0.1;
+echo $camiseta->precoFinal();
+// Resultado: 90
+*/
+
+public class conta {
+    public $preco; public $preco2;
+}
